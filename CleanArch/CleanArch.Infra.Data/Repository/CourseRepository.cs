32890b91d7b3e669a330f4b9a431e80fe0e6462a
Joel.Pineda <you@example.com>
2021-07-21 03:42:03 +0800

    Commands. CommandHandlers. Updated Course Repository. Cleaned up IOC

--- CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -25,5 +25,11 @@ namespace CleanArch.Infra.Data.Repository
         {
             return _ctx.Courses;
         }
+
+        public void Add(Course course)
+        {
+            _ctx.Courses.Add(course);
+            _ctx.SaveChanges();  
+        }
     }
 }
